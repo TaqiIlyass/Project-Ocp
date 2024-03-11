85936de54d5855a802952914e5451b36a70dcfcf
TaqiIlyass <ilyasstaqi7@gmail.com>
2024-03-11 17:12:53 +0000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
